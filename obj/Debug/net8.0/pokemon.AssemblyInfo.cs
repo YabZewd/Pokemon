4d53a4f83739b87e1e4d067c5ce061134f491479
YabZewd <yabse.z@gmail.com>
2025-01-10 19:04:41 +0300

    fixed vulnarability

--- obj/Debug/net8.0/pokemon.AssemblyInfo.cs
+++ obj/Debug/net8.0/pokemon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85323af7cc1310bbf1c337d33a9b4eaebcd5bdc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
